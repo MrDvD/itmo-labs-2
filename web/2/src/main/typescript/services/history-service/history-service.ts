53d0d60b8a095000fb024a0a5682fb8222c7b1d8
MrDvD <ma.labin@yandex.ru>
2025-09-14 14:01:39 +0300

    feat: update form validation & ajax via get query

--- web/2/src/main/typescript/services/history-service/history-service.ts
+++ web/2/src/main/typescript/services/history-service/history-service.ts
@@ -1,43 +1,16 @@
-export enum HistoryServiceConstants {
-  storageKey = "lab-dot-status-history",
-  historyTableSelector = ".lab-query-history > table"
-}
-
 export class HistoryService {
-  // private isEmpty = false;
-  // private storageKey: string
+  private tableSelector: string
 
-  // public constructor(storageKey: string) {
-  //   this.storageKey = storageKey;
-  // }
+  public constructor(tableSelector: string = ".lab-query-history") {
+    this.tableSelector = tableSelector;
+  }
 
-  public fillHistoryTable(historyTable: HTMLTableElement): void {
-    const tbodyElement = historyTable.querySelector("tbody");
-    if (tbodyElement === null) {
+  public fillHistoryTable(outerHTML: string): void {
+    const historyTable = document.querySelector(this.tableSelector);
+    if (!historyTable) {
       console.error("Не удалось найти тело таблицы.");
-      return;
+      return
     }
-    // if (this.isEmpty && dotStatuses.length > 0) {
-    //   tbodyElement.deleteRow(0);
-    //   this.isEmpty = false;
-    // }
-    // for (const dotStatus of dotStatuses) {
-    //   const newRow = tbodyElement.insertRow(0);
-    //   const cellX = newRow.insertCell();
-    //   cellX.innerText = dotStatus.entry.X;
-    //   const cellY = newRow.insertCell();
-    //   cellY.innerText = String(dotStatus.entry.Y);
-    //   const cellR = newRow.insertCell();
-    //   cellR.innerText = dotStatus.entry.R;
-    //   const cellResult = newRow.insertCell();
-    //   cellResult.innerText = dotStatus.hit ? "да" : "нет";
-    //   const cellDate = newRow.insertCell();
-    //   cellDate.innerText = dotStatus.date;
-    //   const cellDuration = newRow.insertCell();
-    //   cellDuration.innerText = dotStatus.duration;
-    // }
-    // if (saveToHistory) {
-    //   this.appendToHistory(dotStatuses);
-    // }
+    historyTable.outerHTML = outerHTML;
   }
 }
