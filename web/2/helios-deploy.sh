dfda1b9a006ec7da1f7c289809b8de229082df39
MrDvD <ma.labin@yandex.ru>
2025-09-15 12:45:32 +0300

    fix: replace existing files via ssh

--- web/2/helios-deploy.sh
+++ web/2/helios-deploy.sh
@@ -1,4 +1,5 @@
-rm -rf public && \
-npm run format && \
-npm run build && \
+rm -rf public
+npm run format
+npm run build
+ssh helios "rm -r wildfly-37.0.1.Final/resources"
 scp -r public helios:wildfly-37.0.1.Final/resources
