515502be0ae231da6ac456b3be8d73609887b4a4
MrDvD <ma.labin@yandex.ru>
2025-10-16 15:45:11 +0300

    feat: parametrization of InRangeValidator

--- web/3.1/jsf-component/src/main/scala/validator/RRangeValidator.scala
+++
@@ -1,6 +0,0 @@
-package com.itmo.mrdvd.validator
-
-import jakarta.faces.validator.FacesValidator
-
-@FacesValidator
-class RRangeValidator extends InRangeValidator(1, 3)
--- web/3.1/jsf-component/src/main/scala/validator/XRangeValidator.scala
+++
@@ -1,6 +0,0 @@
-package com.itmo.mrdvd.validator
-
-import jakarta.faces.validator.FacesValidator
-
-@FacesValidator
-class XRangeValidator extends InRangeValidator(-3, 5)
