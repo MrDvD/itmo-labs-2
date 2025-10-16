43a3026f3c1da210beeeffdc625a2f0749c99343
MrDvD <ma.labin@yandex.ru>
2025-10-17 00:51:16 +0300

    fix: working ajax form sending

--- web/3.1/app/src/main/scala/com/itmo/mrdvd/listener/PointResultListener.scala
+++ web/3.1/app/src/main/scala/com/itmo/mrdvd/listener/PointResultListener.scala
@@ -2,15 +2,16 @@ package com.itmo.mrdvd.listener
 
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
-import com.itmo.mrdvd.event.PointResultEvent
 import com.itmo.mrdvd.dto.DotResult
 import jakarta.faces.context.FacesContext
+import java.util.logging.Logger
+import com.itmo.mrdvd.dto.PointResultEvent
 
 @ApplicationScoped
 class PointResultListener:
   val idsToUpdate = Seq("plot-area:dots", "dot-history")
 
   def onPointResult(@Observes event: PointResultEvent[DotResult]): Unit =
-    val ctx = event.getFacesContext()
+    val ctx = FacesContext.getCurrentInstance()
     for (id <- idsToUpdate) do
       ctx.getPartialViewContext().getRenderIds().add(id)
--- web/3.1/jsf-component/src/main/scala/event/PointResultEvent.scala
+++
@@ -1,7 +0,0 @@
-package com.itmo.mrdvd.event
-
-import jakarta.faces.event.ComponentSystemEvent
-import jakarta.faces.component.UIComponent
-
-class PointResultEvent[T](component: UIComponent, result: T)
-    extends ComponentSystemEvent(component)
