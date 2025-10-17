ae14c4a23d6914908e889147dabbbf25d7643dba
MrDvD <ma.labin@yandex.ru>
2025-10-17 18:58:40 +0300

    fix: update paths in vds deploy script

--- web/3.1/vds-deploy.sh
+++ web/3.1/vds-deploy.sh
@@ -16,13 +16,13 @@ ssh -o ControlPath=$ssh_ctl vdska << EOF
 EOF
 WEB_3_1=$(ssh -o ControlPath=$ssh_ctl vdska 'source .bashrc; echo $WEB_3_1')
 scp -ro ControlPath=$ssh_ctl public "vdska:$WEB_3_1" && \
-scp -ro ControlPath=$ssh_ctl target "vdska:$WEB_3_1" && \
+scp -ro ControlPath=$ssh_ctl app/target "vdska:$WEB_3_1"/app && \
 ssh -o ControlPath=$ssh_ctl vdska << EOF
   source .bashrc
   cd \$WEB_3_1
   docker compose down
-  chown -R 1000:1000 ./target ./public ./src
-  chmod -R u+rwX ./target ./public ./src
+  chown -R 1000:1000 ./app ./public
+  chmod -R u+rwX ./app ./public
   POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB POSTGRES_USER=$POSTGRES_USER POSTGRES_PASSWORD=$POSTGRES_PASSWORD docker compose up
 EOF
 ssh -o ControlPath=$ssh_ctl -O exit vdska
