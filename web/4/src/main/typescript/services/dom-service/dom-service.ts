e12503ddbba468af33529af33ec983b70b0561a1
MrDvD <ma.labin@yandex.ru>
2025-10-24 11:01:40 +0300

    refactor: TS reconfig

--- web/4/src/main/typescript/services/dom-service/dom-service.ts
+++
@@ -1,9 +0,0 @@
-export class DomService {
-  public getCanvas(canvasId = "plot-area"): HTMLElement {
-    const canvas = document.getElementById(canvasId);
-    if (canvas === null) {
-      throw new Error("Не удалось найти изображение для рендера точек.");
-    }
-    return canvas;
-  }
-}
---
+++ web/4/src/main/webapp/dots/script.ts
@@ -0,0 +1,5 @@
+import DotsPage from '/components/pages/dots/DotsPage.svelte';
+
+window.initialData = {
+  currentPageComponent: DotsPage,
+};
