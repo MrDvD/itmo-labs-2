ee425c9c9f27a1c28f8fc83705e5a7731e59ca4a
MrDvD <ma.labin@yandex.ru>
2025-10-24 10:48:58 +0300

    feat: rewrite most components to Svelte

---
+++ web/4/src/main/webapp/components/dot-history/DotHistoryComponent.svelte
@@ -0,0 +1,17 @@
+<style>
+  @import 'style.css';
+</style>
+
+<table class="query-history">
+  <caption>
+    <b>История запросов</b>
+  </caption>
+  <thead>
+    <tr class="header-row"><td>X</td><td>Y</td><td>R</td><td>Результат</td><td>Дата</td></tr>
+  </thead>
+  <tbody class="query-history-body">
+    <tr>
+      <td colspan="100">Нет запросов!</td>
+    </tr>
+  </tbody>
+</table>
