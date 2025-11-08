242e1b074e592c1efb776934d3f17686dc4435ac
MrDvD <ma.labin@yandex.ru>
2025-11-08 15:56:30 +0300

    feat: sending form on img click

--- web/4/src/main/webapp/components/forms/plot-form/PlotFormComponent.svelte
+++ web/4/src/main/webapp/components/forms/plot-form/PlotFormComponent.svelte
@@ -6,20 +6,25 @@
 <script lang="ts">
   import { AppServices } from 'lib/services.js';
   import { main as generic_main, handleSubmit } from '../script.js';
-  import { main } from './script.js';
+  import { main, fillCoords } from './script.js';
 
   generic_main();
   main();
 
   const dotsRepository = AppServices.DOTS_REPOSITORY.get();
+  let form;
 
   function myHandleSubmit(event: Event) {
     handleSubmit(event, dotsRepository);
   }
+
+  function myFillCoords(event: MouseEvent) {
+    fillCoords(form, event);
+  }
 </script>
 
-<form class="lab-form" on:submit|preventDefault={myHandleSubmit}>
-  <img src="/resources/png/plot.png" alt="Dot plot" draggable="false" />
+<form class="lab-form" bind:this={form} on:submit|preventDefault={myHandleSubmit}>
+  <img src="/resources/png/plot.png" on:click={myFillCoords} alt="Dot plot" draggable="false" />
   <div id="dots"></div>
   <input type="hidden" name="X" />
   <input type="hidden" name="Y" />
--- web/4/src/main/webapp/components/forms/plot-form/script.ts
+++ web/4/src/main/webapp/components/forms/plot-form/script.ts
@@ -1,3 +1,44 @@
+export function fillCoords(form: HTMLFormElement, event: MouseEvent) {
+  const canvas = event.target as unknown as HTMLElement;
+  const scale = canvas.offsetWidth * 3 / 8
+  const R = getR(form);
+  if (R == null || isNaN(R)) {
+    console.error("wawaw!!!");
+    return;
+  }
+  const inputX = form.querySelector("input[name=X]") as HTMLInputElement | null;
+  if (inputX == null) {
+    console.error("Could not found hidden input X element.");
+    return;
+  }
+  const inputY = form.querySelector("input[name=Y]") as HTMLInputElement | null;
+  if (inputY == null) {
+    console.error("Could not found hidden input Y element.");
+    return;
+  }
+  const X = 
+    Math.round(
+      ((event.offsetX - canvas.offsetWidth / 2) * 100 * R) /
+        scale,
+    ) / 100;
+  const Y =
+    Math.round(
+      ((event.offsetY - canvas.offsetHeight / 2) * -100 * R) /
+        scale,
+    ) / 100;
+  inputX.value = String(X);
+  inputY.value = String(Y);
+  form.requestSubmit();
+}
+
+function getR(form: HTMLFormElement): number | null {
+  const input = form.querySelector("input[name=R]") as HTMLInputElement | null;
+  if (input == null) {
+    return null;
+  }
+  return input.valueAsNumber;
+}
+
 export function main() {
   document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll(".dynamic-slider").forEach((slider) => {
@@ -5,7 +46,12 @@ export function main() {
       if (track == null) {
         return;
       }
-      slider.querySelector("input[type=range]")?.addEventListener("input", (event) => {
+      const sliderInput = slider.querySelector("input[type=range]") as HTMLInputElement | null;
+      if (sliderInput == null) {
+        return;
+      }
+      track.innerHTML = sliderInput.value;
+      sliderInput.addEventListener("input", (event) => {
         const target = event.target;
         if (target instanceof HTMLInputElement) {
           track.innerHTML = target.value;
