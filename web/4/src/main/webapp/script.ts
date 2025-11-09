f57b67859f84075e0407a60a889fee807aa7ff37
MrDvD <ma.labin@yandex.ru>
2025-11-09 11:37:25 +0300

    fix: pages' script does not fall with error
    mounting all components on single page

--- web/4/src/main/webapp/script.ts
+++ web/4/src/main/webapp/script.ts
@@ -1,5 +1,40 @@
+import { mount } from 'svelte'
 import MainPage from '@components/pages/main/MainPage.svelte';
+import DotsPage from '@components/pages/dots/DotsPage.svelte';
 
-window.initialData = {
-  currentPageComponent: MainPage,
-};
+function main() {
+  const app = document.getElementById('app');
+  if (!app) {
+    throw new Error('App root element was not found');
+  }
+
+  const routes = {
+    '/': {
+      title: "Start",
+      page: MainPage,
+    },
+    '/dots/': {
+      title: "Main",
+      page: DotsPage,
+    }
+  };
+
+  const currentPath = window.location.pathname;
+  let currentPageComponent;
+  if (currentPath in routes) {
+    currentPageComponent = routes[currentPath as keyof typeof routes];
+  } else {
+    window.alert("Unknown page! Please check URL for typos");
+  }
+
+  if (currentPageComponent === undefined) {
+    throw new Error(`No component found for path: ${currentPath}`);
+  }
+
+  document.title = currentPageComponent.title;
+  mount(currentPageComponent.page, {
+    target: app,
+  });
+}
+
+main();
