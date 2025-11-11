f18bccf192be81076fd00e6a33b4767ad0347dce
MrDvD <ma.labin@yandex.ru>
2025-11-11 22:57:21 +0300

    feat: start updating database

--- web/4/src/main/scala/com/itmo/mrdvd/mapper/DotResultMapper.scala
+++ web/4/src/main/scala/com/itmo/mrdvd/mapper/DotResultMapper.scala
@@ -9,20 +9,18 @@ class DotResultMapper extends Mapper[Dot, DotResult]:
   val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
   val zone = ZoneId.of("Europe/Moscow")
   override def apply(dot: Dot): Either[DotResult, Error] =
-    val inCircle = dot.X >= 0 && dot.Y >= 0 && Math.pow(dot.X, 2) + Math.pow(
+    val inCircle = dot.X <= 0 && dot.Y <= 0 && Math.pow(dot.X, 2) + Math.pow(
       dot.Y,
       2
     ) <= Math.pow(dot.R / 2, 2)
-    val inSquare =
-      Math.min(dot.X, -dot.Y) >= 0 && Math.max(dot.X, -dot.Y) <= dot.R
-    val inTriangle =
-      dot.X <= 0 && dot.X >= -dot.R && dot.Y >= 0 && 2 * dot.Y <= dot.X + dot.R
+    val inRectangle = -dot.R / 2 <= dot.X && dot.X <= 0 && 0 <= dot.Y && dot.Y <= dot.R
+    val inTriangle = 0 <= dot.X && dot.X <= dot.R && 0 <= dot.Y && dot.Y <= dot.R / 2 && dot.Y <= -dot.X / 2 + dot.R / 2
     roundDotMapper(dot) match
       case Left(value) =>
         Left(
           DotResult(
             value,
-            inCircle || inSquare || inTriangle,
+            inCircle || inRectangle || inTriangle,
             ZonedDateTime.now(zone).format(formatter)
           )
         )
