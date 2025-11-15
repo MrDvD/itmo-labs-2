2f4ef016eb50b24c439c3c3fd4c57ea96005733a
MrDvD <ma.labin@yandex.ru>
2025-11-16 00:09:49 +0300

    refactor: update deploy scripts

--- web/4/vds-deploy.sh
+++ web/4/vds-deploy.sh
@@ -1,9 +1,8 @@
 npm i && \
 scalafmt && \
-mvn clean package && \
-npm run clean && \
-npm run format && \
-npm run build
+npm run clean
+npm run build && \
+mvn clean package
 
 ssh_ctl=./ssh-ctl
 
@@ -12,17 +11,18 @@ ssh -o ControlPath=$ssh_ctl vdska << EOF
   source .bashrc
   rm -rf \$ITMO_LABS/itmo-labs-2
   cd \$ITMO_LABS
-  git clone git@github.com:MrDvD/itmo-labs-2.git --branch web-3-dop
+  git clone git@github.com:MrDvD/itmo-labs-2.git --branch web-4
 EOF
-WEB_3_1=$(ssh -o ControlPath=$ssh_ctl vdska 'source .bashrc; echo $WEB_3_1')
-scp -ro ControlPath=$ssh_ctl public "vdska:$WEB_3_1" && \
-scp -ro ControlPath=$ssh_ctl app/target "vdska:$WEB_3_1"/app && \
+WEB_4=$(ssh -o ControlPath=$ssh_ctl vdska 'source .bashrc; echo $WEB_4')
+scp -ro ControlPath=$ssh_ctl dist "vdska:$WEB_4" && \
+scp -ro ControlPath=$ssh_ctl target "vdska:$WEB_4"/app && \
 ssh -o ControlPath=$ssh_ctl vdska << EOF
   source .bashrc
-  cd \$WEB_3_1
+  cd \$WEB_4
   docker compose down
-  chown -R 1000:1000 ./app ./public
-  chmod -R u+rwX ./app ./public
-  POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB POSTGRES_USER=$POSTGRES_USER POSTGRES_PASSWORD=$POSTGRES_PASSWORD docker compose up
+  cp ./docker/Dockerfile.main ./Dockerfile && \
+  chown -R 1000:1000 ./dist ./target
+  chmod -R u+rwX ./dist ./target
+  POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB POSTGRES_USER=$POSTGRES_USER POSTGRES_PASSWORD=$POSTGRES_PASSWORD APP_SECRET=$APP_SECRET docker compose up
 EOF
 ssh -o ControlPath=$ssh_ctl -O exit vdska
