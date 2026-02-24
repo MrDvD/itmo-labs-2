80da340022cf0ccc9dea98ad7a4f653c91cbbbd2
MrDvD <ma.labin@yandex.ru>
2026-02-24 16:02:23 +0300

    feat: add linear solution for 1296

--- asd/timus/1296/main.cpp
+++ asd/timus/1296/main.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
-#include <vector>
 
 int main() {
   int N;
   std::cin >> N;
-  std::vector<int> arr;
-  std::vector<long long> prefix;
-  prefix.push_back(0);
+  long long ans = 0, curr = 0;
   for (int i = 0; i < N; i++) {
-    arr.push_back(0);
-    prefix.push_back(0);
-  }
-  for (int i = 0; i < N; i++) {
-    std::cin >> arr[i];
-    prefix[i + 1] = prefix[i] + arr[i];
-  }
-  long long ans = 0;
-  for (int i = 1; i <= N; i++) {
-    for (int j = i; j <= N; j++) {
-      long long curr = prefix[j] - prefix[i - 1];
-      if (curr > ans) {
-        ans = curr;
-      }
+    int item;
+    std::cin >> item;
+    curr += item;
+    if (curr < 0) {
+      curr = 0;
+    }
+    if (curr > ans) {
+      ans = curr;
     }
   }
   std::cout << ans;
