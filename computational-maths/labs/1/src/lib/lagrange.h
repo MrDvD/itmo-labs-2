37036702dba8bb5f363edfe62d386724dabf2ce2
MrDvD <ma.labin@yandex.ru>
2026-03-12 18:28:59 +0300

    feat: done lab 1

--- computational-maths/labs/1/src/lib/lagrange.h
+++ computational-maths/labs/1/src/lib/lagrange.h
@@ -9,5 +9,5 @@ struct debug_result {
 };
 
 void fill_chebyshev_polynomial_roots(const int n, const double a, const double b, double roots[]);
-std::function<double(double)> generate_lagrange_polynomial(const int n, const std::function<double(double)> func, const double grid[]);
+std::function<double(double)> generate_lagrange_polynomial(const int n, const double values[], const double grid[]);
 debug_result interpolate_by_lagrange(int f, double a, double b, double x);
--- computational-maths/labs/1/src/main.cpp
+++ computational-maths/labs/1/src/main.cpp
@@ -5,39 +5,38 @@
 #include <lib/test.h>
 
 int main() {
-  std::ofstream csv_file("interpolation_results.csv");
+  std::ofstream interpolation_file("pgf/interpolation_results.csv");
+  std::ofstream plots_file("pgf/function_plots.csv");
 
-  csv_file << "f_id,a,b,x,y_interp,y_actual,y_noise,residual,n\n";
+  interpolation_file << "f_id,x,y,residual,n\n";
+  plots_file << "f_id,x,y\n";
 
   std::vector<test_case> cases = generate_tests(0.3);
-  for (int i = 0; i < 1; i++) {
+  int current_func_num = 0;
+  for (std::size_t i = 0; i < cases.size(); i++) {
     test_case c = cases[i];
-    std::function<double(double)> func = get_function(c.function_number);
-    if (!c.calc) {
-      csv_file << c.function_number << "," 
-              << std::nan << "," 
-              << std::nan << "," 
-              << c.x << "," 
-              << std::nan << "," 
-              << func(c.x) << "," 
-              << std::nan << ","
-              << std::nan << "\n";
-      continue;
+    if (c.function_number != current_func_num) {
+      current_func_num = c.function_number;
     }
-    debug_result debug = interpolate_by_lagrange(c.function_number, c.a, c.b, c.x);
-    double y_actual = func(c.x);
-    double error = y_actual - debug.result;
-    csv_file << c.function_number << "," 
-             << c.a << "," 
-             << c.b << "," 
-             << c.x << "," 
-             << debug.result << "," 
-             << y_actual << "," 
-             << error << ","
-             << debug.end_iteration << "\n";
+    std::function<double(double)> func = get_function(c.function_number);
+    
+    plots_file << c.function_number << "," 
+               << c.x << "," 
+               << func(c.x) << "\n";
+    if (c.calc) {
+      debug_result debug = interpolate_by_lagrange(c.function_number, c.a, c.b, c.x);
+      double y_actual = func(c.x);
+      double error = y_actual - debug.result;
+      interpolation_file << c.function_number << ","
+                         << c.x << ","
+                         << debug.result << "," 
+                         << error << ","
+                         << debug.end_iteration << "\n";
+    }
   }
 
-  csv_file.close();
+  interpolation_file.close();
+  plots_file.close();
 
   return 0;
 }
