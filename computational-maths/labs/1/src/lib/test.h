cea80af3f806b708830ca448439bb91f2785c9ee
MrDvD <ma.labin@yandex.ru>
2026-03-12 12:58:48 +0300

    feat: conclusion is done (almost)
    start writing tests

---
+++ computational-maths/labs/1/src/lib/test.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <random>
+#include <algorithm>
+
+struct test_case {
+  int function_number;
+  double x, a, b;
+  bool calc;
+};
+
+std::vector<test_case> generate_tests(const double threshold);
