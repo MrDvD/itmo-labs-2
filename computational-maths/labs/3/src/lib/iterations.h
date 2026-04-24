63a6dd60064eba86eb3cd42441136a4002050dc7
MrDvD <ma.labin@yandex.ru>
2026-04-25 01:28:08 +0300

    feat: done lab 3

--- computational-maths/labs/3/src/lib/iterations.h
+++
@@ -1,9 +0,0 @@
-#ifndef ITERATIONS_H
-#define ITERATIONS_H
-
-#include "preamble.h"
-#include <unordered_map>
-
-vector<double> solve_by_fixed_point_iterations(int system_id, int number_of_unknowns, vector<double> x);
-
-#endif
