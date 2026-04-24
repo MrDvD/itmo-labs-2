3415b484458e983f7a8f7efbecad91f844bf6f37
MrDvD <ma.labin@yandex.ru>
2026-04-24 19:37:35 +0300

    chore: save progress on lab 3

---
+++ computational-maths/labs/3/src/lib/iterations.h
@@ -0,0 +1,9 @@
+#ifndef ITERATIONS_H
+#define ITERATIONS_H
+
+#include "preamble.h"
+#include <unordered_map>
+
+vector<double> solve_by_fixed_point_iterations(int system_id, int number_of_unknowns, vector<double> x);
+
+#endif
---
+++ computational-maths/labs/3/src/main.cpp
@@ -0,0 +1,9 @@
+#include "lib/preamble.h"
+#include "lib/gauss.h"
+#include "lib/input.h"
+#include "lib/output.h"
+#include "lib/random.h"
+
+int main() {
+  
+}
