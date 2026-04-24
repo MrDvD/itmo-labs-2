dae4b2b6406d5fc505699cc214317bbe4aa978dc
MrDvD <ma.labin@yandex.ru>
2026-04-25 01:28:08 +0300

    feat: done lab 3

--- computational-maths/labs/2/src/main.cpp
+++ computational-maths/labs/2/src/main.cpp
@@ -1,7 +1,7 @@
-#include "lib/preamble.h"
 #include "lib/gauss.h"
 #include "lib/input.h"
 #include "lib/output.h"
+#include "lib/preamble.h"
 #include "lib/random.h"
 
 int main() {
--- computational-maths/labs/3/src/lib/iterations.hpp
+++ computational-maths/labs/3/src/lib/iterations.hpp
@@ -1,8 +1,3 @@
-\subsection{Листинг численного метода}
-
-Для реализации блок-схем воспользуемся языком программирования C++.
-
-\begin{lstlisting}[caption=Файл \texttt{iterations.hpp}]
 #ifndef ITERATIONS_H
 #define ITERATIONS_H
 
@@ -132,39 +127,3 @@ std::vector<double> CaluclateResidues(const std::size_t system_num, const std::v
 }  // namespace
 
 #endif
-\end{lstlisting}
-
-\begin{lstlisting}[caption=Файл \texttt{main.cpp}]
-#include "lib/iterations.hpp"
-
-#include <iostream>
-#include <unordered_map>
-
-int main() {
-  SimpleIterationsSystem obj = InitSystem();
-
-  std::unordered_map<std::size_t, std::vector<double>> initial_x = {
-    {1, { 0.0, 0.0 }},
-    {2, { -1.9, 2.1 }},
-    {3, { 0.0, 0.0, 0.0 }},
-    {4, { 0.8, 0.6 }},
-    {5, { 0.4, 0.2 }},
-  };
-  for (std::size_t i = 1; i <= 5; i++) {
-    std::cout << "--- Solution for system #" << i << ":\n";
-    ResultInfo rawInfo = obj.SolveSystem(i, initial_x[i]);
-    std::vector<double> result = rawInfo.solution;
-    std::vector<double> residues = CaluclateResidues(i, result);
-    for (std::size_t j = 0; j < result.size(); j++) {
-      std::cout << "x[" << j << "]: " << result[j] << "\t\t" << "res[" << j << "]: " << residues[j] << "\n";
-    }
-    double mse = 0.0;
-    for (const auto& res : residues) {
-      mse += res * res;
-    }
-    mse /= residues.size();
-    std::cout << "MSE: " << mse << "\n";
-    std::cout << "Total iterations: " << rawInfo.iterations_count << "\n";
-  }
-}
-\end{lstlisting}
