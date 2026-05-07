ba5aabfe7284d2309307521ae83835d63789a3b5
MrDvD <ma.labin@yandex.ru>
2026-05-07 17:29:35 +0300

    feat: start writing report for lab 4

--- computational-maths/labs/4/src/lib/integration.hpp
+++ computational-maths/labs/4/src/lib/integration.hpp
@@ -1,15 +1,18 @@
 #ifndef INTEGRATION_H
 #define INTEGRATION_H
 
-#include "preamble.h"
+#include <limits>
 
+#include "preamble.h"
 #include "unordered_map"
 #include "vector"
-#include <limits>
 
 struct DiscontinuityDot {
   double x;
-  bool is_second_order;
+  // 0 - continuous
+  // 1 - first order
+  // 2 - second order
+  std::size_t order;
 };
 
 struct DiscontinuityPlace {
@@ -32,16 +35,22 @@ struct IntegrationResult {
 
 class IntegrateFunctions {
 public:
-  IntegrateFunctions(double epsilon) : IntegrateFunctions(epsilon, 1e6, std::unordered_map<std::size_t, FunctionMeta>{}) {
+  IntegrateFunctions(double epsilon)
+      : IntegrateFunctions(epsilon, 1e6, std::unordered_map<std::size_t, FunctionMeta>{}) {
   }
 
-  IntegrateFunctions(double epsilon, std::size_t max_iterations, std::unordered_map<std::size_t, FunctionMeta> funcs) : _funcs(funcs), _epsilon(epsilon), _max_iterations(max_iterations) {
+  IntegrateFunctions(
+      double epsilon,
+      std::size_t max_iterations,
+      std::unordered_map<std::size_t, FunctionMeta> funcs
+  )
+      : _funcs(funcs), _epsilon(epsilon), _max_iterations(max_iterations) {
   }
 
   void AddFunction(const std::size_t number, const FunctionMeta& func) {
     this->_funcs[number] = func;
   }
-  
+
   IntegrationResult Integrate(std::size_t number, double a, double b) {
     double result_sign = 1.0;
     if (b < a) {
@@ -53,73 +62,84 @@ public:
     }
     double result = 0.0;
     FunctionMeta meta = this->_funcs[number];
-    std::vector<double> split_points = {a};
+    std::vector<DiscontinuityDot> split_points = {
+        {a, 0}
+    };
 
     for (const auto& place : meta.discontinuity_places) {
       if (place.is_interval) {
-        if (place.left.x < b && place.right.x > a) {
-          return {0.0, false, true, "Integrated function has discontinuity or does not defined in current interval"};
+        if (place.left.x < b && a < place.right.x) {
+          return {
+              0.0,
+              false,
+              true,
+              "Integrated function has discontinuity or does not defined in current interval"};
         }
       } else {
         double dx = place.left.x;
         if (dx >= a && dx <= b) {
-          if (place.left.is_second_order) {
-            return {0.0, false, true, "Integrated function has discontinuity or does not defined in current interval"};
+          if (place.left.order == 2) {
+            return {
+                0.0,
+                false,
+                true,
+                "Integrated function has discontinuity or does not defined in current interval"};
           }
           if (dx != a && dx != b) {
-            split_points.push_back(dx);
+            split_points.push_back({dx, place.left.order});
           }
         }
       }
     }
-    split_points.push_back(b);
+    split_points.push_back({b, 0});
 
     double total_result = 0.0;
     for (std::size_t i = 0; i < split_points.size() - 1; ++i) {
-      double sub_a = split_points[i];
-      double sub_b = split_points[i + 1];
+      DiscontinuityDot sub_a = split_points[i];
+      DiscontinuityDot sub_b = split_points[i + 1];
       total_result += _IntegrateSubInterval(meta.f, sub_a, sub_b);
     }
 
     return {
-      total_result * result_sign,
-      true,
-      false,
+        total_result * result_sign,
+        true,
+        false,
     };
   }
 
 private:
-  double _IntegrateSubInterval(const std::function<double(double)>& f, double a, double b) {
-    auto safe_eval = [&](double x, double h_step) -> double {
-      double val = f(x);
-      if (std::isnan(val) || std::isinf(val)) {
+  double _IntegrateSubInterval(
+      const std::function<double(double)>& f, const DiscontinuityDot& a, const DiscontinuityDot& b
+  ) {
+    auto safe_eval = [&](double x, double h_step, bool is_left, bool is_continuous) -> double {
+      if (!is_continuous) {
         double eps = std::max(1e-12, h_step * 0.01);
-        double v_l = f(x - eps);
-        double v_r = f(x + eps);
-        bool bad_l = std::isnan(v_l) || std::isinf(v_l);
-        bool bad_r = std::isnan(v_r) || std::isinf(v_r);
-        
-        if (!bad_l && !bad_r) {
-          return (v_l + v_r) * 0.5;
+        if (is_left) {
+          double v_r = f(x + eps);
+          bool bad_r = std::isnan(v_r) || std::isinf(v_r);
+          if (!bad_r) {
+            return v_r;
+          }
         }
+
+        double v_l = f(x - eps);
+        bool bad_l = std::isnan(v_l) || std::isinf(v_l);
         if (!bad_l) {
           return v_l;
         }
-        if (!bad_r) {
-          return v_r;
-        }
         return 0.0;
       }
-      return val;
+      return f(x);
     };
     double prev_res = 0.0;
     std::size_t n = 1;
 
     while (n <= this->_max_iterations) {
-      double h = (b - a) / n;
-      double current_res = 0.5 * (safe_eval(a, h) + safe_eval(b, h));
+      double h = (b.x - a.x) / n;
+      double current_res =
+          0.5 * (safe_eval(a.x, h, true, a.order == 0) + safe_eval(b.x, h, false, b.order == 0));
       for (std::size_t i = 1; i < n; i++) {
-        current_res += f(a + i * h);
+        current_res += f(a.x + i * h);
       }
       current_res *= h;
 
@@ -139,54 +159,66 @@ private:
 
 IntegrateFunctions InitMain(double epsilon) {
   IntegrateFunctions integrate = IntegrateFunctions(epsilon);
-  integrate.AddFunction(1, FunctionMeta{
-    get_function(1),
-    {
-      DiscontinuityPlace{
-        false,
-        {
-          0.0,
-          true,
-        },
-      },
-    },
-  });
-  integrate.AddFunction(2, FunctionMeta{
-    get_function(2),
-    {
-      DiscontinuityPlace{
-        false,
-        {
-          0.0,
-          false,
-        },
-      },
-    },
-  });
-  integrate.AddFunction(3, FunctionMeta{
-    get_function(3),
-    {},
-  });
-  integrate.AddFunction(4, FunctionMeta{
-    get_function(4),
-    {},
-  });
-  integrate.AddFunction(5, FunctionMeta{
-    get_function(5),
-    {
-      DiscontinuityPlace{
-        true,
-        {
-          -std::numeric_limits<double>::infinity(),
-          false,
-        },
-        {
-          0.0,
-          true,
-        },
-      },
-    },
-  });
+  integrate.AddFunction(
+      1,
+      FunctionMeta{
+          get_function(1),
+          {
+               DiscontinuityPlace{
+               false,
+               {
+               0.0,
+               2,
+               },
+               }, },
+  }
+  );
+  integrate.AddFunction(
+      2,
+      FunctionMeta{
+          get_function(2),
+          {
+               DiscontinuityPlace{
+               false,
+               {
+               0.0,
+               1,
+               },
+               }, },
+  }
+  );
+  integrate.AddFunction(
+      3,
+      FunctionMeta{
+          get_function(3),
+          {},
+      }
+  );
+  integrate.AddFunction(
+      4,
+      FunctionMeta{
+          get_function(4),
+          {},
+      }
+  );
+  integrate.AddFunction(
+      5,
+      FunctionMeta{
+          get_function(5),
+          {
+               DiscontinuityPlace{
+               true,
+               {
+               -std::numeric_limits<double>::infinity(),
+               0,
+               },
+               {
+               0.0,
+               2,
+               },
+               }, },
+  }
+  );
   return integrate;
 }
 
--- computational-maths/labs/4/src/main.cpp
+++ computational-maths/labs/4/src/main.cpp
@@ -1,17 +1,17 @@
-#include "lib/integration.hpp"
-
 #include <iostream>
 #include <unordered_map>
+
+#include "lib/integration.hpp"
 
 int main() {
   IntegrateFunctions obj = InitMain(1e-6);
 
   std::unordered_map<std::size_t, std::vector<double>> bounds = {
-    {1, { 0.0, 0.0 }},
-    {2, { -1.9, 2.1 }},
-    {3, { 0.0, 2.0 }},
-    {4, { 0.8, 0.6 }},
-    {5, { 0.4, 0.2 }},
+      {1,  {0.0, 0.0}},
+      {2, {-1.9, 2.1}},
+      {3,  {0.0, 2.0}},
+      {4,  {0.8, 0.6}},
+      {5,  {0.4, 0.2}},
   };
   for (std::size_t i = 1; i <= 5; i++) {
     std::cout << "--- Solution for function #" << i << ":\n";
