a34d2e1f20f3c7494c18cff80f0cb74547a97024
MrDvD <ma.labin@yandex.ru>
2026-05-20 21:06:39 +0300

    feat: done task 2

--- optimization-methods/7/src/lib/optimize.py
+++ optimization-methods/7/src/lib/optimize.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List, Tuple, Protocol, runtime_checkable
+from numpy.typing import NDArray
 from abc import ABC
 
 @runtime_checkable
@@ -21,7 +22,7 @@ class GradientDescentOptimizer(BaseOptimizer):
     self.max_iter = max_iter
     self.eps = eps
   
-  def _get_gradient(self, params: np.ndarray) -> np.ndarray:
+  def _get_gradient(self, params: NDArray[np.float32]) -> np.ndarray:
     x, y = params[0], params[1]
     df_dx = 10**-2 * (16 * x + 2 * y + 43)
     df_dy = 10**-2 * (2 * x + 10)
@@ -35,6 +36,38 @@ class GradientDescentOptimizer(BaseOptimizer):
       if np.linalg.norm(grad) < self.eps:
         break
       next_params = params - self.learning_rate * grad
+      self._callback(tuple(next_params))
+      if np.linalg.norm(next_params - params) < self.eps:
+        params = next_params
+        break
+      params = next_params
+    return self.iter_history
+
+class OurAdamOptimizer(BaseOptimizer):
+  def __init__(self, learning_rate: float, max_iter: int, eps: float) -> None:
+    super().__init__()
+    self.learning_rate = learning_rate
+    self.max_iter = max_iter
+    self.eps = eps
+  
+  def _get_gradient(self, x: float, y: float) -> NDArray[np.float32]:
+    df_dx = 10**-2 * (16 * x + 2 * y + 43)
+    df_dy = 10**-2 * (2 * x + 10)
+    return np.array([df_dx, df_dy], dtype=float)
+
+  def optimize(self, params_start: NDArray[np.float32]) -> List[Tuple[float, float]]:
+    x0, y0, beta1, beta2 = params_start
+    params = np.array([x0, y0])
+    self.iter_history = [(x0, y0)]
+    velocity = 0.0
+    mass = 0.0
+    for t in range(1, self.max_iter + 1):
+      grad = self._get_gradient(*self.iter_history[-1])
+      if np.linalg.norm(grad) < self.eps:
+        break
+      mass = beta1 * mass + (1 - beta1) * grad
+      velocity: float = beta2 * velocity + (1 - beta2) * grad ** 2
+      next_params = params - self.learning_rate / np.sqrt(velocity / (1.0 - beta1 ** t)) * mass / (1 - beta1 ** t)
       self._callback(tuple(next_params))
       if np.linalg.norm(next_params - params) < self.eps:
         params = next_params
--- optimization-methods/7/src/main.py
+++ optimization-methods/7/src/main.py
@@ -1,7 +1,8 @@
 import lib.config as config 
 from lib.plot import Visualizer
-from lib.optimize import GradientDescentOptimizer
+from lib.optimize import GradientDescentOptimizer, OurAdamOptimizer
 from lib.report import ReportFiller
+from numpy.typing import NDArray
 import os
 import numpy as np
 
@@ -9,13 +10,15 @@ if __name__ == "__main__":
   cfg = config.load()
   
   pics_dir = os.path.join(cfg['report_dir'], "pics")
+  x_range = (-20, 20)
+  y_range = (-50, 50)
   plot = Visualizer(
-    x_range=(-20, 20),
-    y_range=(-50, 50),
+    x_range=x_range,
+    y_range=y_range,
     output_dir=pics_dir
   )
 
-  def function(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+  def function(x: NDArray[np.float64], y: NDArray[np.float64]) -> NDArray[np.float64]:
     return 10 ** (-2) * (8 * x ** 2 + 2 * x * y + 43 * x + 10 * y + 15)
   
   saddle_points = (np.array([-5.0]), np.array([18.5]))
@@ -28,12 +31,23 @@ if __name__ == "__main__":
   gd_iterations = gd_optimizer.optimize(start_param)
   plot.plot_optimization_trajectory(function, gd_iterations, saddle_points, min_points, 20, "gd_iterations.pdf")
 
+  adam_optimizer = OurAdamOptimizer(cfg['adam_learning_rate'], len(gd_iterations) - 1, cfg['adam_eps'])
+  adam_iterations = adam_optimizer.optimize(np.array([*start_param, cfg['adam_beta_1'], cfg['adam_beta_2']]))
+  plot.plot_optimization_trajectory(function, adam_iterations, saddle_points, min_points, 20, 'adam_iterations.pdf')
+
   report = ReportFiller({
     'gd_init': f"({start_param[0]}, {start_param[1]})^T",
     'gd_learning_rate': cfg['gd_learning_rate'],
     'max_iter': cfg['max_iter'],
     'gd_eps': f"{cfg['gd_eps']:.5f}",
     'gd_iterations_num': str(len(gd_iterations) - 1),
+    'adam_init': f"({start_param[0]}, {start_param[1]})^T",
+    'adam_learning_rate': cfg['adam_learning_rate'],
+    'adam_iterations': str(len(gd_iterations) - 1),
+    'adam_beta_1': cfg['adam_beta_1'],
+    'adam_beta_2': cfg['adam_beta_2'],
+    'adam_eps': f"{cfg['adam_eps']:.5f}",
+    'adam_iterations_num': str(len(adam_iterations) - 1),
   }, cfg['report_dir'])
   
   report.compile_patterns()
